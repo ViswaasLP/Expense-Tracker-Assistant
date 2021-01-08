08264419fb417399470d9215b439698b39703129
ViswaasLP <viswaas98@gmail.com>
2021-01-08 15:49:30 -0800

    Ready to deploy

--- expense/src/components/Main/Main.jsx
+++ expense/src/components/Main/Main.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
 import useStyles from './styles'
 import Form from './Form/Form'
 import List from './List/List'
-
+import {ExpenseTrackerContext} from '../../context/context'
+import InfoCard from '../InfoCard'
 
 const Main = () => {
     const classes = useStyles()
+    const {balance} = useContext(ExpenseTrackerContext)
     return (
 
-        <Card classes = {classes.root}>
+        <Card classes = {classes.root} style = {{background:'rgba(255,255,255,0.65)'}}>
             <CardHeader title = 'Expense Tracker' subheader = 'Powered by Speechly'/>
             <CardContent>
-                <Typography align = 'center' variant = 'h5'>Total Balance: $500</Typography>
+                <Typography align = 'center' variant = 'h5'>Total Balance: ${balance}</Typography>
                 <Typography variant = 'subtitle1' style = {{lineHeight: '1.5em', marginTop: '20px'}}>
-                    {/* Infocard */}
+                    <InfoCard />
                 </Typography>
                 <Divider />
                 <Form />
